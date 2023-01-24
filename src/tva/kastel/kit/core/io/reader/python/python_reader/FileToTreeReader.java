468e1c9a04da2cf852f0bc903e3525521903d75d
David Bumm <uusiy@student.kit.edu>
2023-01-24 10:23:30 +0100

    parsing different python files works

--- src/tva/kastel/kit/core/io/reader/python/python_reader/FileToTreeReader.java
+++ src/tva/kastel/kit/core/io/reader/python/python_reader/FileToTreeReader.java
@@ -4,308 +4,34 @@ import jep.Interpreter;
 import jep.SharedInterpreter;
 
 public class FileToTreeReader {
-	
-	
-	public String getTreeFromFileMocked(String path) {
-		return "{\n" +
-				"    \"_type\": \"Module\",\n" +
-				"    \"body\": [\n" +
-				"        {\n" +
-				"            \"_type\": \"Import\",\n" +
-				"            \"col_offset\": 0,\n" +
-				"            \"end_col_offset\": 11,\n" +
-				"            \"end_lineno\": 1,\n" +
-				"            \"lineno\": 1,\n" +
-				"            \"names\": [\n" +
-				"                {\n" +
-				"                    \"_type\": \"alias\",\n" +
-				"                    \"asname\": null,\n" +
-				"                    \"col_offset\": 7,\n" +
-				"                    \"end_col_offset\": 11,\n" +
-				"                    \"end_lineno\": 1,\n" +
-				"                    \"lineno\": 1,\n" +
-				"                    \"name\": \"json\"\n" +
-				"                }\n" +
-				"            ]\n" +
-				"        },\n" +
-				"        {\n" +
-				"            \"_type\": \"ImportFrom\",\n" +
-				"            \"col_offset\": 0,\n" +
-				"            \"end_col_offset\": 21,\n" +
-				"            \"end_lineno\": 2,\n" +
-				"            \"level\": 0,\n" +
-				"            \"lineno\": 2,\n" +
-				"            \"module\": \"ast\",\n" +
-				"            \"names\": [\n" +
-				"                {\n" +
-				"                    \"_type\": \"alias\",\n" +
-				"                    \"asname\": null,\n" +
-				"                    \"col_offset\": 16,\n" +
-				"                    \"end_col_offset\": 21,\n" +
-				"                    \"end_lineno\": 2,\n" +
-				"                    \"lineno\": 2,\n" +
-				"                    \"name\": \"parse\"\n" +
-				"                }\n" +
-				"            ]\n" +
-				"        },\n" +
-				"        {\n" +
-				"            \"_type\": \"ImportFrom\",\n" +
-				"            \"col_offset\": 0,\n" +
-				"            \"end_col_offset\": 29,\n" +
-				"            \"end_lineno\": 3,\n" +
-				"            \"level\": 0,\n" +
-				"            \"lineno\": 3,\n" +
-				"            \"module\": \"ast2json\",\n" +
-				"            \"names\": [\n" +
-				"                {\n" +
-				"                    \"_type\": \"alias\",\n" +
-				"                    \"asname\": null,\n" +
-				"                    \"col_offset\": 21,\n" +
-				"                    \"end_col_offset\": 29,\n" +
-				"                    \"end_lineno\": 3,\n" +
-				"                    \"lineno\": 3,\n" +
-				"                    \"name\": \"ast2json\"\n" +
-				"                }\n" +
-				"            ]\n" +
-				"        },\n" +
-				"        {\n" +
-				"            \"_type\": \"Assign\",\n" +
-				"            \"col_offset\": 0,\n" +
-				"            \"end_col_offset\": 92,\n" +
-				"            \"end_lineno\": 5,\n" +
-				"            \"lineno\": 5,\n" +
-				"            \"targets\": [\n" +
-				"                {\n" +
-				"                    \"_type\": \"Name\",\n" +
-				"                    \"col_offset\": 0,\n" +
-				"                    \"ctx\": {\n" +
-				"                        \"_type\": \"Store\"\n" +
-				"                    },\n" +
-				"                    \"end_col_offset\": 3,\n" +
-				"                    \"end_lineno\": 5,\n" +
-				"                    \"id\": \"ast\",\n" +
-				"                    \"lineno\": 5\n" +
-				"                }\n" +
-				"            ],\n" +
-				"            \"type_comment\": null,\n" +
-				"            \"value\": {\n" +
-				"                \"_type\": \"Call\",\n" +
-				"                \"args\": [\n" +
-				"                    {\n" +
-				"                        \"_type\": \"Call\",\n" +
-				"                        \"args\": [\n" +
-				"                            {\n" +
-				"                                \"_type\": \"Call\",\n" +
-				"                                \"args\": [],\n" +
-				"                                \"col_offset\": 21,\n" +
-				"                                \"end_col_offset\": 90,\n" +
-				"                                \"end_lineno\": 5,\n" +
-				"                                \"func\": {\n" +
-				"                                    \"_type\": \"Attribute\",\n" +
-				"                                    \"attr\": \"read\",\n" +
-				"                                    \"col_offset\": 21,\n" +
-				"                                    \"ctx\": {\n" +
-				"                                        \"_type\": \"Load\"\n" +
-				"                                    },\n" +
-				"                                    \"end_col_offset\": 88,\n" +
-				"                                    \"end_lineno\": 5,\n" +
-				"                                    \"lineno\": 5,\n" +
-				"                                    \"value\": {\n" +
-				"                                        \"_type\": \"Call\",\n" +
-				"                                        \"args\": [\n" +
-				"                                            {\n" +
-				"                                                \"_type\": \"Constant\",\n" +
-				"                                                \"col_offset\": 26,\n" +
-				"                                                \"end_col_offset\": 82,\n" +
-				"                                                \"end_lineno\": 5,\n" +
-				"                                                \"kind\": null,\n" +
-				"                                                \"lineno\": 5,\n" +
-				"                                                \"n\": \"C:/Users/david/Documents/Informatik/Hiwi/JsonParser.py\",\n" +
-				"                                                \"s\": \"C:/Users/david/Documents/Informatik/Hiwi/JsonParser.py\",\n" +
-				"                                                \"value\": \"C:/Users/david/Documents/Informatik/Hiwi/JsonParser.py\"\n" +
-				"                                            }\n" +
-				"                                        ],\n" +
-				"                                        \"col_offset\": 21,\n" +
-				"                                        \"end_col_offset\": 83,\n" +
-				"                                        \"end_lineno\": 5,\n" +
-				"                                        \"func\": {\n" +
-				"                                            \"_type\": \"Name\",\n" +
-				"                                            \"col_offset\": 21,\n" +
-				"                                            \"ctx\": {\n" +
-				"                                                \"_type\": \"Load\"\n" +
-				"                                            },\n" +
-				"                                            \"end_col_offset\": 25,\n" +
-				"                                            \"end_lineno\": 5,\n" +
-				"                                            \"id\": \"open\",\n" +
-				"                                            \"lineno\": 5\n" +
-				"                                        },\n" +
-				"                                        \"keywords\": [],\n" +
-				"                                        \"lineno\": 5\n" +
-				"                                    }\n" +
-				"                                },\n" +
-				"                                \"keywords\": [],\n" +
-				"                                \"lineno\": 5\n" +
-				"                            }\n" +
-				"                        ],\n" +
-				"                        \"col_offset\": 15,\n" +
-				"                        \"end_col_offset\": 91,\n" +
-				"                        \"end_lineno\": 5,\n" +
-				"                        \"func\": {\n" +
-				"                            \"_type\": \"Name\",\n" +
-				"                            \"col_offset\": 15,\n" +
-				"                            \"ctx\": {\n" +
-				"                                \"_type\": \"Load\"\n" +
-				"                            },\n" +
-				"                            \"end_col_offset\": 20,\n" +
-				"                            \"end_lineno\": 5,\n" +
-				"                            \"id\": \"parse\",\n" +
-				"                            \"lineno\": 5\n" +
-				"                        },\n" +
-				"                        \"keywords\": [],\n" +
-				"                        \"lineno\": 5\n" +
-				"                    }\n" +
-				"                ],\n" +
-				"                \"col_offset\": 6,\n" +
-				"                \"end_col_offset\": 92,\n" +
-				"                \"end_lineno\": 5,\n" +
-				"                \"func\": {\n" +
-				"                    \"_type\": \"Name\",\n" +
-				"                    \"col_offset\": 6,\n" +
-				"                    \"ctx\": {\n" +
-				"                        \"_type\": \"Load\"\n" +
-				"                    },\n" +
-				"                    \"end_col_offset\": 14,\n" +
-				"                    \"end_lineno\": 5,\n" +
-				"                    \"id\": \"ast2json\",\n" +
-				"                    \"lineno\": 5\n" +
-				"                },\n" +
-				"                \"keywords\": [],\n" +
-				"                \"lineno\": 5\n" +
-				"            }\n" +
-				"        },\n" +
-				"        {\n" +
-				"            \"_type\": \"Expr\",\n" +
-				"            \"col_offset\": 0,\n" +
-				"            \"end_col_offset\": 32,\n" +
-				"            \"end_lineno\": 6,\n" +
-				"            \"lineno\": 6,\n" +
-				"            \"value\": {\n" +
-				"                \"_type\": \"Call\",\n" +
-				"                \"args\": [\n" +
-				"                    {\n" +
-				"                        \"_type\": \"Call\",\n" +
-				"                        \"args\": [\n" +
-				"                            {\n" +
-				"                                \"_type\": \"Name\",\n" +
-				"                                \"col_offset\": 17,\n" +
-				"                                \"ctx\": {\n" +
-				"                                    \"_type\": \"Load\"\n" +
-				"                                },\n" +
-				"                                \"end_col_offset\": 20,\n" +
-				"                                \"end_lineno\": 6,\n" +
-				"                                \"id\": \"ast\",\n" +
-				"                                \"lineno\": 6\n" +
-				"                            }\n" +
-				"                        ],\n" +
-				"                        \"col_offset\": 6,\n" +
-				"                        \"end_col_offset\": 31,\n" +
-				"                        \"end_lineno\": 6,\n" +
-				"                        \"func\": {\n" +
-				"                            \"_type\": \"Attribute\",\n" +
-				"                            \"attr\": \"dumps\",\n" +
-				"                            \"col_offset\": 6,\n" +
-				"                            \"ctx\": {\n" +
-				"                                \"_type\": \"Load\"\n" +
-				"                            },\n" +
-				"                            \"end_col_offset\": 16,\n" +
-				"                            \"end_lineno\": 6,\n" +
-				"                            \"lineno\": 6,\n" +
-				"                            \"value\": {\n" +
-				"                                \"_type\": \"Name\",\n" +
-				"                                \"col_offset\": 6,\n" +
-				"                                \"ctx\": {\n" +
-				"                                    \"_type\": \"Load\"\n" +
-				"                                },\n" +
-				"                                \"end_col_offset\": 10,\n" +
-				"                                \"end_lineno\": 6,\n" +
-				"                                \"id\": \"json\",\n" +
-				"                                \"lineno\": 6\n" +
-				"                            }\n" +
-				"                        },\n" +
-				"                        \"keywords\": [\n" +
-				"                            {\n" +
-				"                                \"_type\": \"keyword\",\n" +
-				"                                \"arg\": \"indent\",\n" +
-				"                                \"col_offset\": 22,\n" +
-				"                                \"end_col_offset\": 30,\n" +
-				"                                \"end_lineno\": 6,\n" +
-				"                                \"lineno\": 6,\n" +
-				"                                \"value\": {\n" +
-				"                                    \"_type\": \"Constant\",\n" +
-				"                                    \"col_offset\": 29,\n" +
-				"                                    \"end_col_offset\": 30,\n" +
-				"                                    \"end_lineno\": 6,\n" +
-				"                                    \"kind\": null,\n" +
-				"                                    \"lineno\": 6,\n" +
-				"                                    \"n\": 4,\n" +
-				"                                    \"s\": 4,\n" +
-				"                                    \"value\": 4\n" +
-				"                                }\n" +
-				"                            }\n" +
-				"                        ],\n" +
-				"                        \"lineno\": 6\n" +
-				"                    }\n" +
-				"                ],\n" +
-				"                \"col_offset\": 0,\n" +
-				"                \"end_col_offset\": 32,\n" +
-				"                \"end_lineno\": 6,\n" +
-				"                \"func\": {\n" +
-				"                    \"_type\": \"Name\",\n" +
-				"                    \"col_offset\": 0,\n" +
-				"                    \"ctx\": {\n" +
-				"                        \"_type\": \"Load\"\n" +
-				"                    },\n" +
-				"                    \"end_col_offset\": 5,\n" +
-				"                    \"end_lineno\": 6,\n" +
-				"                    \"id\": \"print\",\n" +
-				"                    \"lineno\": 6\n" +
-				"                },\n" +
-				"                \"keywords\": [],\n" +
-				"                \"lineno\": 6\n" +
-				"            }\n" +
-				"        }\n" +
-				"    ],\n" +
-				"    \"type_ignores\": []\n" +
-				"}";	}
-	
-	
-	public String getTreeFromFileAsString(String path) {
-		try (Interpreter interp = new SharedInterpreter()) {
-			 interp.exec("import ast");
-			 interp.exec("file = open(\"" + path + "\", \"r\")");
-			 interp.exec("data = file.read()");
-			 interp.exec("file.close()");
-			 interp.exec("tree = ast.parse(data)");
-			 interp.exec("printTree = ast.dump(tree)");
-			 Object object =  interp.getValue("printTree");
-			 
-			 return object.toString();
-		}
 
-	}
-	
-	public String getTreeFromFileAsJSON(String path) {
-		try (Interpreter interp = new SharedInterpreter()) {
-			 interp.exec("import json");
-			 interp.exec("from ast import parse");
-			 interp.exec("from ast2json import ast2json");
-			 interp.exec("ast = ast2json(parse(open(\"C:/Users/david/Documents/Informatik/Hiwi/commands.py\").read()))");
-			 interp.exec("tree = json.dumps(ast, indent=4)");
-			 Object object = interp.getValue("tree");
-			 
-			 return object.toString();
-		}
+    public String getTreeFromFileAsString(String path) {
+        try (Interpreter interp = new SharedInterpreter()) {
+            interp.exec("import ast");
+            interp.exec("file = open(\"" + path + "\", \"r\")");
+            interp.exec("data = file.read()");
+            interp.exec("file.close()");
+            interp.exec("tree = ast.parse(data)");
+            interp.exec("printTree = ast.dump(tree)");
+            Object object = interp.getValue("printTree");
 
-	}
+            return object.toString();
+        }
+
+    }
+
+    public String getTreeFromFileAsJSON(String path) {
+        try (Interpreter interp = new SharedInterpreter()) {
+            interp.exec("import json");
+            interp.exec("from ast import parse");
+            interp.exec("from ast2json import ast2json"); //Required: "pip install ast2json"
+            interp.exec("ast = ast2json(parse(open(" + path + ").read()))");
+            interp.exec("tree = json.dumps(ast, indent=4)");
+            Object object = interp.getValue("tree");
+
+            return object.toString();
+        }
+
+    }
 
 }
--- src/tva/kastel/kit/core/io/reader/python/python_reader/PythonFileReader.java
+++ src/tva/kastel/kit/core/io/reader/python/python_reader/PythonFileReader.java
@@ -5,8 +5,6 @@ import java.io.File;
 import java.nio.file.Paths;
 import java.util.Map;
 
-
-
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -23,111 +21,94 @@ import tva.kastel.kit.core.model.interfaces.Tree;
 
 public class PythonFileReader extends AbstractArtifactReader {
 
-	public static String[] SUPPORTED_FILE_ENDINGS = { Const.PY };
+    public static String[] SUPPORTED_FILE_ENDINGS = {Const.PY};
 
-	private final FileToTreeReader fileToTree;
+    private final FileToTreeReader fileToTree;
 
-	public PythonFileReader() {
-		super(SUPPORTED_FILE_ENDINGS);
-		fileToTree = new FileToTreeReader();
-	}
+    public PythonFileReader() {
+        super(SUPPORTED_FILE_ENDINGS);
+        fileToTree = new FileToTreeReader();
+    }
 
-	@Override
-	public Tree readArtifact(File element) {
-		String fileName = Paths.get(element.getAbsolutePath()).getFileName().toString();
-		return readArtifact(element, fileName);
-	}
+    @Override
+    public Tree readArtifact(File element) {
+        String fileName = Paths.get(element.getAbsolutePath()).getFileName().toString();
+        return readArtifact(element, fileName);
+    }
 
-	public Tree readArtifact(File element, String rootName) {
-		Node rootNode = new NodeImpl(Const.PYTHON);
-		String path = element.getAbsolutePath();
-		path = Const.QUOTATION + path.replace(Const.BACKSLASH, Const.DIVIDE_OP) + Const.QUOTATION ;
-		
+    public Tree readArtifact(File element, String rootName) {
+        Node rootNode = new NodeImpl(Const.PYTHON);
+        String path = element.getAbsolutePath();
+        path = Const.QUOTATION + path.replace(Const.BACKSLASH, Const.DIVIDE_OP) + Const.QUOTATION;
 
-		Gson gson = new Gson();
-		JsonObject obj = gson.fromJson(fileToTree.getTreeFromFileMocked(path), JsonObject.class);
+        Gson gson = new Gson();
+        JsonObject obj = gson.fromJson(fileToTree.getTreeFromFileAsJSON(path), JsonObject.class);
 
-		generateNodes(obj, rootNode, gson);
+        generateNodes(obj, rootNode, gson);
 
-		/*
-		 * TODO: if (element.isDirectory()) { rootNode = createDirectory(element,
-		 * rootName); for (FileTreeElement childElement : element.getChildren()) {
-		 * createHierarchy(childElement, rootNode); }
-		 * 
-		 * } else { if (isFileSupported(element.getExtension())) { rootNode =
-		 * readArtifactRoot(element); } else { rootNode = createFile(element); }
-		 * 
-		 * }
-		 */
-		
-		//adjust the tree so it can be compared to other programming languages
-		PAdjustAll PAdjustAll = new PAdjustAll();
-		PAdjustAll.adjustAll(rootNode);
-		
-		//get file name without extension
-		String name = element.getName().substring(0, element.getName().length() - 3);
-		rootNode.getChildren().get(0).addAttribute(Const.NAME_BIG, name);
+        //adjust the tree so it can be compared to other programming languages
+        PAdjustAll PAdjustAll = new PAdjustAll();
+        PAdjustAll.adjustAll(rootNode);
 
-		return new TreeImpl(element.getName(), rootNode);
+        //get file name without extension
+        String name = element.getName().substring(0, element.getName().length() - 3);
+        rootNode.getChildren().get(0).addAttribute(Const.NAME_BIG, name);
 
-	}
-	
-	
-	private void generateNodes(JsonArray jsonArr, Node rootNode, Gson gson) {
-		for (int i = 0; i < jsonArr.size(); i++) {
-			JsonObject obj = gson.fromJson(jsonArr.get(i).toString(), JsonObject.class);
-			generateNodes(obj, rootNode, gson);
-		}
-	}
+        return new TreeImpl(element.getName(), rootNode);
 
-	private void generateNodes(JsonObject obj, Node rootNode, Gson gson) {
-		JsonObject jsonObj = gson.toJsonTree(obj).getAsJsonObject();
-		Node node = null;
-		for (Map.Entry<String, JsonElement> entry : jsonObj.entrySet()) {
-			if (entry.getKey().equals(Const._TYPE)) {
-				node = new NodeImpl(getName(entry.getValue()), rootNode);
-			} else if (node != null) {
-				try {
-					createNodefromJsonObject(gson, node, entry.getValue().toString(), entry.getKey());
-				} catch (JsonSyntaxException e) {
-					try {
-						createNodefromJsonArray(gson, node, entry.getValue().toString(), entry.getKey());
-					} catch (JsonSyntaxException b) {
-						addAttribute(node, entry.getKey(), entry.getValue());
-					}
-				}
+    }
 
-			}
-		}
-	}
-	
-	private void createNodefromJsonObject(Gson gson, Node node, String value, String  entry) {
-		JsonArray jsonArr = gson.fromJson(value, JsonArray.class);
-		Node nextNode = new NodeImpl(entry, node);
-		generateNodes(jsonArr, nextNode, gson);
-	}
-	
-	private void createNodefromJsonArray(Gson gson, Node node, String value, String  entry) {
-		JsonObject nextObj = gson.fromJson(value, JsonObject.class);
-		Node nextNode = new NodeImpl(entry, node);
-		generateNodes(nextObj, nextNode, gson);
-	}
-	
-	
-	
-	private void addAttribute(Node node, String entry, JsonElement value ) {
-		if (!Const.BANED_ATTRIBUTES.contains(entry)) {
-			node.addAttribute(entry, getName(value));
-		}
-	}
+    private void generateNodes(JsonArray jsonArr, Node rootNode, Gson gson) {
+        for (int i = 0; i < jsonArr.size(); i++) {
+            JsonObject obj = gson.fromJson(jsonArr.get(i).toString(), JsonObject.class);
+            generateNodes(obj, rootNode, gson);
+        }
+    }
 
+    private void generateNodes(JsonObject obj, Node rootNode, Gson gson) {
+        JsonObject jsonObj = gson.toJsonTree(obj).getAsJsonObject();
+        Node node = null;
+        for (Map.Entry<String, JsonElement> entry : jsonObj.entrySet()) {
+            if (entry.getKey().equals(Const._TYPE)) {
+                node = new NodeImpl(getName(entry.getValue()), rootNode);
+            } else if (node != null) {
+                try {
+                    createNodefromJsonObject(gson, node, entry.getValue().toString(), entry.getKey());
+                } catch (JsonSyntaxException e) {
+                    try {
+                        createNodefromJsonArray(gson, node, entry.getValue().toString(), entry.getKey());
+                    } catch (JsonSyntaxException b) {
+                        addAttribute(node, entry.getKey(), entry.getValue());
+                    }
+                }
 
+            }
+        }
+    }
 
-	private String getName(JsonElement element) {
-		if (element.toString().length() > 2) {
-			return element.toString().substring(1, element.toString().length() - 1);
-		}
-		return element.toString();
-	}
+    private void createNodefromJsonObject(Gson gson, Node node, String value, String entry) {
+        JsonArray jsonArr = gson.fromJson(value, JsonArray.class);
+        Node nextNode = new NodeImpl(entry, node);
+        generateNodes(jsonArr, nextNode, gson);
+    }
+
+    private void createNodefromJsonArray(Gson gson, Node node, String value, String entry) {
+        JsonObject nextObj = gson.fromJson(value, JsonObject.class);
+        Node nextNode = new NodeImpl(entry, node);
+        generateNodes(nextObj, nextNode, gson);
+    }
+
+    private void addAttribute(Node node, String entry, JsonElement value) {
+        if (!Const.BANED_ATTRIBUTES.contains(entry)) {
+            node.addAttribute(entry, getName(value));
+        }
+    }
+
+    private String getName(JsonElement element) {
+        if (element.toString().length() > 2) {
+            return element.toString().substring(1, element.toString().length() - 1);
+        }
+        return element.toString();
+    }
 
 }
